54512facd2f2cace2d1eaf9052ec68b5b38d6745
OmerCeyhan <mrceyhan96@gmail.com>
2017-04-22 14:37:46 +0300

    Add files via upload

---
+++ GumrukOtomation/src/java/Beans/User.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+/**
+ *
+ * @author Brainiac
+ */
+@ManagedBean(name="user")
+@SessionScoped
+public class User {
+    private int id;
+    private String isim;
+    private String soyisim;
+    private String email;
+    private String kullaniciAdi;
+    private String sifre;
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyisim() {
+        return soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKullaniciAdi() {
+        return kullaniciAdi;
+    }
+
+    public void setKullaniciAdi(String kullaniciAdi) {
+        this.kullaniciAdi = kullaniciAdi;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+    
+}
