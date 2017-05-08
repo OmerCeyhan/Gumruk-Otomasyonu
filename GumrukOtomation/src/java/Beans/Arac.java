1ef0ac3bb618b1283e8dc552443a337a3642bb18
OmerCeyhan <mrceyhan96@gmail.com>
2017-05-08 16:11:40 +0300

    Add files via upload

--- GumrukOtomation/src/java/Beans/Arac.java
+++ GumrukOtomation/src/java/Beans/Arac.java
@@ -10,90 +10,37 @@ import javax.faces.bean.SessionScoped;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
- import java.sql.PreparedStatement;
- import java.sql.ResultSet;
- import java.sql.SQLException;
- import javax.annotation.Resource;
- import javax.faces.bean.ManagedBean;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
- import javax.sql.DataSource;
- import javax.sql.rowset.CachedRowSet;
+import javax.sql.DataSource;
+import javax.sql.rowset.CachedRowSet;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author Brainiac
  */
-@ManagedBean(name="arac")
+@ManagedBean(name = "arac")
 @RequestScoped
 public class Arac {
-    DataSource dataSource;
-    
+
     private String plaka;
     private String aracCinsi;
     private String ulkesi;
     private String firma;
     private String model;
     //jdbc:derby://localhost:1527/gumrukotomasyon [APP on APP]
-    public Arac() throws SQLException, ClassNotFoundException{
-          try {
-			Context ctx = new InitialContext();
-                        //Class.forName("org.apache.derby.jdbc.ClientDriver");
-                       // DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-			dataSource = (DataSource)ctx.lookup("jdbc:derby:gumrukotomasyon");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-    }
 
-     public String aracEkle() throws SQLException, NamingException {
-       
-        // check whether dataSource was injected by the server
-        if (dataSource == null) {
-            throw new SQLException("Unable to obtain DataSource");
-        }
-
-        // obtain a connection from the connection pool
-        Connection connection = dataSource.getConnection();
-
-        // check whether connection was successful
-        if (connection == null) {
-            throw new SQLException("Unable to connect to DataSource");
-        }
-
-        try {
-            // create a PreparedStatement to insert a new address book entry
-            PreparedStatement addEntry
-                    = connection.prepareStatement("INSERT INTO ARAC"
-                            + "(plaka,aracCinsi,ulkesi,firma,model)"
-                            + "VALUES ( ?, ?, ?, ?,? )");
-
-            // specify the PreparedStatement's arguments
-            addEntry.setString(1, getPlaka());
-            addEntry.setString(2, getAracCinsi());
-            addEntry.setString(3, getUlkesi());
-            addEntry.setString(4, getFirma());
-            addEntry.setString(5, getModel());
-
-            addEntry.executeUpdate(); // insert the entry
-            return "home"; // go back to home.xhtml home page
-        } // end try
-        finally {
-            connection.close(); // return this connection to pool
-        } // end finally
-    }
-    
-     public DataSource getDataSource() {
-        return dataSource;
-    }
-
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-    
     public String getPlaka() {
         return plaka;
     }
@@ -133,6 +80,31 @@ public class Arac {
     public void setModel(String model) {
         this.model = model;
     }
-    
-   
+
+    public String aracEkle() throws SQLException, NamingException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gumruk", "root", "omer1996");
+
+            String sql = "INSERT INTO ARAC (plaka, arac_Cinsi, ulkesi, firma, model) values(?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, getPlaka());
+            ps.setString(2, getAracCinsi());
+            ps.setString(3, getUlkesi());
+            ps.setString(4, getFirma());
+            ps.setString(5, getModel());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        
+        return "aracbilgi";
+    }
 }
